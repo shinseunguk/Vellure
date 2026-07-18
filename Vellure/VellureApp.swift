5583ceccae780cfa761be90e343b34d816fe4122
shinseunguk <krdut1@gmail.com>
2026-07-18 14:37:40 +0900

    Initial Commit

---
+++ Vellure/VellureApp.swift
@@ -0,0 +1,17 @@
+//
+//  VellureApp.swift
+//  Vellure
+//
+//  Created by ukBook on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct VellureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
